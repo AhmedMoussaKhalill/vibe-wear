be7d1e8fdebe68e0d314471ca7e81236a0a6e5b3
ziad atef <zatef301@gmail.com>
2024-10-06 17:07:42 +0300

    sign in

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-
+import Login from "./components/Login";
 const App = () => {
-  return <div>Hello React</div>;
+  return <div className=" flex w-full h-screen">
+    <div className="w-full flex items-center justify-center lg:w-full bg-gray-100">
+    <Login/>
+    </div>
+ 
+  
+    </div>;
 };
 
 export default App;
