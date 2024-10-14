ad8ce9596a10fe5cfdd2d7dd1af001d7edb200c6
AhmedMoussaKhalill <147548727+AhmedMoussaKhalill@users.noreply.github.com>
2024-10-14 21:52:40 +0300

    Update LayoutUser.jsx

--- src/LayoutUser.jsx
+++ src/LayoutUser.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import UserHeader from "./components/user/UserHeader";
 import UserProfile from "./pages/UserProfile";
+import Shop from "./Components/Shop/Shop";
+import Cart from "./Components/Cart/Cart";
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 
-const LayoutUser = ({ users, logged, setLogged, userDetails, setUserDetails }) => {
+const LayoutUser = ({ users, logged, setLogged, userDetails, setUserDetails, CartArray, setCartArray }) => {
   return (
     <div>
       <UserHeader
@@ -16,6 +18,14 @@ const LayoutUser = ({ users, logged, setLogged, userDetails, setUserDetails }) =
         <Route
           path="/profile"
           element={logged && <UserProfile userDetails={userDetails} setUserDetails={setUserDetails} />}
+        />
+        <Route
+          path="/shop"
+          element={<Shop CartArray={CartArray} setCartArray={setCartArray} /> />}
+        />
+        <Route
+          path="/cart"
+          element={<Cart CartArray={CartArray} setCartArray={setCartArray} />}
         />
         <Route
           path="/login"
