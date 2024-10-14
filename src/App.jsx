bc5500b37a69e350cfd87e8b9fac77a7847468e0
ziad atef <zatef301@gmail.com>
2024-10-14 13:26:14 +0300

    finally

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Login from './components/Login';
+import SignUp from './components/SignUp'; 
 
-const App = () => {
-  return <div>Hello React</div>;
+function App() {
+  return (
+    <div className="flex h-screen w-full bg-gray-200">
+      <div className="flex w-full items-center justify-center bg-gray-300 lg:w-full">
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} /> 
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
+    </div>
+    </div>
+
+  )
+  
 };
+  
 
-export default App;
+
+export default App;
