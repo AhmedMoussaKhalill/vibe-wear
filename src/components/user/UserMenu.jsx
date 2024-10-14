13d00ec90cc935e673edf88b0495818a5eefc285
AhmedMoussaKhalill <147548727+AhmedMoussaKhalill@users.noreply.github.com>
2024-10-15 00:11:29 +0300

    Update UserMenu.jsx

--- src/components/user/UserMenu.jsx
+++ src/components/user/UserMenu.jsx
@@ -22,7 +22,7 @@ const UserMenu = ({ setLogged, userDetails }) => {
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <Link to="/profile">
-          <DropdownMenuItem>Welcome Ahmed</DropdownMenuItem>
+          <DropdownMenuItem>Welcome {userDetails?.name}</DropdownMenuItem>
         </Link>
         {userDetails?.role == "admin" && (
           <Link to="/admin">
